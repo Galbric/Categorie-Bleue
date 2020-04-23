40a3c636c8701618144915dbedccd118d10e41bd
Galbric <64203375+Galbric@users.noreply.github.com>
2020-04-23 20:04:46 +0200

    Added openMenu()
    for Mobile-Header only

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const main = document.getElementById("main-project");
 function mainPosition() {
   main.style.top = header.offsetHeight + "px";
 }
+function openMenu() {
+  if (window.innerWidth < 1024) {
+    header.classList.toggle("open");
+  }
+}
