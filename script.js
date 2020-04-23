f038b3456006510c17f8dc0c536ba923c0c5ebff
Galbric <64203375+Galbric@users.noreply.github.com>
2020-04-23 15:41:52 +0200

    Update script.js
    set main.top
    onLoad and onResize ("body")

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const header = document.getElementById("header-bleu");
+const main = document.getElementById("main-project");
 
+function mainPosition() {
+  main.style.top = header.offsetHeight + "px";
+}
