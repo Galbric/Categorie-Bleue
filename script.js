ce82548165c8cf28e13be435394b0318bba9d31d
Galbric <64203375+Galbric@users.noreply.github.com>
2020-05-10 16:49:24 +0200

    Add Gallery Focus for Desktop

--- script.js
+++ script.js
@@ -155,3 +155,117 @@ let prev = photoWrapper.scrollLeft - window.innerWidth / 2.5;
     behavior: 'smooth'
   });
 }
+
+
+
+
+function galleryBig(photo, index) {
+
+  if (window.innerWidth >= 1024) {
+
+    let allPhotos = document.querySelectorAll('.photoWrapper img');
+    let i = index;
+    console.log(i);
+
+    let overlay = document.createElement('div');
+      overlay.style.position = 'absolute';
+      overlay.style.top = '0px';
+      overlay.style.left = '0px';
+      overlay.style.width = '100vw';
+      overlay.style.height = '100vh';
+      overlay.style.background = 'black';
+      overlay.style.opacity = 0.8;
+      overlay.style.zIndex = 200;
+        document.body.appendChild(overlay);
+
+    let photoFocus = document.createElement('img');
+    photoFocus.src = photo.src;
+      photoFocus.style.position = 'absolute';
+      photoFocus.style.top = '50%';
+      photoFocus.style.transform = 'translate(-50%, -50%)';
+      photoFocus.style.left = '50%';
+      photoFocus.style.width = '85vw';
+      photoFocus.style.height = '65vh';
+      photoFocus.style.objectFit = 'contain';
+      photoFocus.style.zIndex = 300;
+      document.body.appendChild(photoFocus);
+
+    let photoCredit = document.createElement('p');
+    photoCredit.innerHTML = allPhotos[i].name;
+      photoCredit.style.fontFamily = "Raleway";
+      photoCredit.style.fontSize = "16px";
+      photoCredit.style.color = '#eee';
+      photoCredit.style.position = 'absolute';
+      photoCredit.style.top = '84vh';
+      photoCredit.style.right = '15vw';
+      photoCredit.style.textAlign = 'right';
+      photoCredit.style.zIndex = 300;
+      document.body.appendChild(photoCredit);
+
+    let previousPhoto = document.createElement('img');
+    previousPhoto.src = 'img/GaucheBlanc.svg';
+      previousPhoto.style.position = 'absolute';
+      previousPhoto.style.top = '50%';
+      previousPhoto.style.left = '1.25vw';
+      previousPhoto.style.width = '5vw';
+      previousPhoto.style.height = '5vw';
+      previousPhoto.style.transform = 'translateY(-50%)';
+      previousPhoto.style.zIndex = 300;
+      previousPhoto.style.cursor = 'pointer';
+      document.body.appendChild(previousPhoto);
+
+      previousPhoto.addEventListener('click', ()=>{
+        if (i != 0) {
+          i--;
+        } else {
+          i = allPhotos.length - 1;
+        }
+        console.log(i);
+        photoFocus.src = allPhotos[i].src;
+        photoCredit.innerHTML = allPhotos[i].name;
+      });
+
+    let nextPhoto = document.createElement('img');
+    nextPhoto.src = 'img/GaucheBlanc.svg';
+      nextPhoto.style.position = 'absolute';
+      nextPhoto.style.top = '50%';
+      nextPhoto.style.right = '1.25vw';
+      nextPhoto.style.width = '5vw';
+      nextPhoto.style.height = '5vw';
+      nextPhoto.style.transform = 'translateY(-50%) rotate(180deg)';
+      nextPhoto.style.zIndex = 300;
+      nextPhoto.style.cursor = 'pointer';
+      document.body.appendChild(nextPhoto);
+
+      nextPhoto.addEventListener('click', ()=>{
+        if (i != allPhotos.length - 1) {
+          i++;
+        } else {
+          i = 0;
+        }
+        console.log(i);
+        photoFocus.src = allPhotos[i].src;
+        photoCredit.innerHTML = allPhotos[i].name;
+      });
+
+    let closeGallery = document.createElement('img');
+    closeGallery.src = 'img/closeGallery.svg';
+      closeGallery.style.position ='absolute';
+      closeGallery.style.top = '5%';
+      closeGallery.style.right = '5%';
+      closeGallery.style.width = '5vw';
+      closeGallery.style.height = '5vw';
+      closeGallery.style.zIndex = 300;
+      closeGallery.style.cursor = 'pointer';
+      document.body.appendChild(closeGallery);
+
+      closeGallery.addEventListener('click', ()=>{
+        overlay.remove();
+        photoFocus.remove();
+        photoCredit.remove();
+        previousPhoto.remove();
+        nextPhoto.remove();
+        closeGallery.remove();
+      });
+  }
+}
